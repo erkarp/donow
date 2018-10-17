af1772f09513e7f6a4ddca49ac441e2ae601c8ea
ekarp <emilyrkarp@gmail.com>
2018-10-17 13:42:42 -0400

    abstract permissions

--- todo.py
+++ todo.py
@@ -3,41 +3,59 @@
 
 import sys
 
-arg = 'view'
-if len(sys.argv) > 1:
-	arg = sys.argv[1]
+PERMISSION = {
+	'add': 'w',
+	'del': 'w',
+	'raw': 'r',
+	'view': 'r', 
+	'clear': 'w'
+} 
 
+arg = 'view' if len(sys.argv) == 1 else sys.argv[1]
+
+file = open('todo.txt', 'r')
+todos = file.read().split('\n')
+file.close()
+
+if PERMISSION[arg] == 'w':
+	file = open('todo.txt', 'w')
+	
+	
+
+
+def print_items(list):
+	for item in list:
+		print(item)
+
+		
 if arg == 'add':
-	file = open('todo.txt','a')
-	new_todos = []
-
+	count = len(todos)
+	
 	while True:
 		print('item todo:')
 		todo = input()
-		
+			
 		if todo == '':
 			break
+				
+		todos.append(str(count) + '. ' + todo)
+		count += count
 			
-		new_todos.append('* ' + todo)
-		
-	file.write('\n'.join(new_todos))
-	
+	file.write('\n'.join(todos))
+
 	
 elif arg == 'clear':
 	if (input('Delete all todos? (y/n)\n') == 'y'):
-		file = open('todo.txt','w')
 		file.write('')
 		
 	
-else: 
-	file = open('todo.txt','r')
-	todos = file.read()
+elif arg == 'view':
+	print_items(todos)
 	
-	if arg == 'view':
-		print(todos)
-		
-	elif arg == 'raw':
-		print(repr(todos))
+	
+elif arg == 'raw':
+	print(repr(todos))
 	
 
-file.close()
+if PERMISSION[arg] == 'w':
+	file.close()
